4f643c2235f8512aae8327f875dc0493395cae07
onizectus <onizectus@gmail.com>
2025-11-17 08:07:04 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import './App.css';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/Blog-Cybersecurity">
       <div className="app">
         <Header />
         <main className="main-content">
